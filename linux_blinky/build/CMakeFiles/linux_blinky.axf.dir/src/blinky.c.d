53827db8d0f487af2b51494692906ec829622d9f
Yusef Karim <yusefkarim@ieee.org>
2017-07-14 21:36:44 -0400

    WOOHOO CMAKE

---
+++ linux_blinky/build/CMakeFiles/linux_blinky.axf.dir/src/blinky.c.d
@@ -0,0 +1,10 @@
+CMakeFiles/linux_blinky.axf.dir/src/blinky.c.o: \
+ /home/fenrir/Documents/tiva-projects/learning-tm4c123/linux_blinky/src/blinky.c \
+ /home/fenrir/Documents/tiva-projects/learning-tm4c123/linux_blinky/src/../inc/tm4c123gh6pm.h \
+ /usr/lib/gcc/arm-none-eabi/7.1.0/include/stdint.h \
+ /usr/arm-none-eabi/include/stdint.h \
+ /usr/arm-none-eabi/include/machine/_default_types.h \
+ /usr/arm-none-eabi/include/sys/features.h \
+ /usr/arm-none-eabi/include/_newlib_version.h \
+ /usr/arm-none-eabi/include/sys/_intsup.h \
+ /usr/arm-none-eabi/include/sys/_stdint.h
---
+++ linux_blinky/build/CMakeFiles/linux_blinky.axf.dir/src/startup.c.d
@@ -0,0 +1,11 @@
+CMakeFiles/linux_blinky.axf.dir/src/startup.c.o: \
+ /home/fenrir/Documents/tiva-projects/learning-tm4c123/linux_blinky/src/startup.c \
+ /usr/lib/gcc/arm-none-eabi/7.1.0/include/stdint.h \
+ /usr/arm-none-eabi/include/stdint.h \
+ /usr/arm-none-eabi/include/machine/_default_types.h \
+ /usr/arm-none-eabi/include/sys/features.h \
+ /usr/arm-none-eabi/include/_newlib_version.h \
+ /usr/arm-none-eabi/include/sys/_intsup.h \
+ /usr/arm-none-eabi/include/sys/_stdint.h \
+ /home/fenrir/Documents/tiva-projects/tivaware/inc/hw_nvic.h \
+ /home/fenrir/Documents/tiva-projects/tivaware/inc/hw_types.h
