6af28bd1fb0117bdeab352522bc7e8ec6485e268
Yusef Karim <yusef-karim@users.noreply.github.com>
2017-07-19 21:51:07 -0400

    blah

--- UTx_Labs/Lab12_TuningFork/TuningFork.c
+++ UTx_Labs/Lab12_TuningFork/TuningFork.c
@@ -39,9 +39,9 @@
 #include "..//tm4c123gh6pm.h"
 
 //Global variables
-unsigned long input;							//Reads PA3 input value
-unsigned long previousInput = 0;	//Remembers switch value from last ISR
-unsigned char nextState = 0;			//Either 0 or 1 depending on input states
+unsigned long input;					//Reads PA3 input value
+unsigned long previous_input = 0;		//Remembers switch value from last ISR
+unsigned char next_state = 0;			//Either 0 or 1 depending on input states
 
 // basic functions defined at end of startup.s
 void DisableInterrupts(void); // Disable interrupts
@@ -73,25 +73,25 @@ void Sound_Init(void)
 	//**** GPIO ****// 
 	volatile unsigned long delay;
 	SYSCTL_RCGC2_R |= 0x00000001;			//A Clock
-	delay = SYSCTL_RCGC2_R;						//delay
+	delay = SYSCTL_RCGC2_R;					//delay
 	GPIO_PORTA_AMSEL_R &= ~0x0C;			//Disable analog functions
-	GPIO_PORTA_PCTL_R &= ~0x0000FF00;	//GPIO PCTL clear bits 3-2 
+	GPIO_PORTA_PCTL_R &= ~0x0000FF00;		//GPIO PCTL clear bits 3-2 
 	GPIO_PORTA_DIR_R &= ~0x08;				//PA3 input
-	GPIO_PORTA_PDR_R |= 0x08;					//Pull-down PA3
-	GPIO_PORTA_DIR_R |= 0x04;					//PA2 output
+	GPIO_PORTA_PDR_R |= 0x08;				//Pull-down PA3
+	GPIO_PORTA_DIR_R |= 0x04;				//PA2 output
 	GPIO_PORTA_AFSEL_R &= ~0x0C;			//Clear alt functions
-	GPIO_PORTA_DEN_R |= 0x0C;					//Enable digital pin PA3-2
+	GPIO_PORTA_DEN_R |= 0x0C;				//Enable digital pin PA3-2
 	
 	//**** SysTick ****//
 	//Clock is set to 80 MHz, each SysTick take 1/80MHz = 12.5 ns
 	//Divide: 80MHz/880Hz = 90909, this is the value we need to pass to reload register
 	
-	NVIC_ST_CTRL_R = 0;										//Disable SysTick during setup
-	NVIC_ST_RELOAD_R = 90909 - 1;					//Number of counts to wait
-	NVIC_ST_CURRENT_R = 0;								//Any write clears current
+	NVIC_ST_CTRL_R = 0;							//Disable SysTick during setup
+	NVIC_ST_RELOAD_R = 90909 - 1;				//Number of counts to wait
+	NVIC_ST_CURRENT_R = 0;						//Any write clears current
 	NVIC_SYS_PRI3_R &= 0x00FFFFFF;				//Give SysTick Priority 0
 	//Enable Systick with core clock
-	//NVIC_ST_CTRL_ENABLE(bit 0)+NVIC_ST_CTRL_CLK_SRC(bit 1)+NVIC_ST_CTRL_INTEN(bit 2)
+	//NVIC_ST_CTRL_ENABLE(bit0)+NVIC_ST_CTRL_CLK_SRC(bit1)+NVIC_ST_CTRL_INTEN(bit2)
 	NVIC_ST_CTRL_R = 0x07;
 	
 }
@@ -104,12 +104,12 @@ void SysTick_Handler(void)
 		input = GPIO_PORTA_DATA_R&0x08;
 		
 		//If previous input was off and current input is high
-		if((previousInput == 0x00) && (input == 0x08))
+		if((previous_input == 0x00) && (input == 0x08))
 			nextState ^= 1;	//Toggle next state
 		
-		if(nextState == 1)
+		if(next_state == 1)
 			GPIO_PORTA_DATA_R ^= 0x04;	//Toggle PA2
 		
-		previousInput = input;	//Remember current ISR input value for later
+		previous_input = input;	//Remember current ISR input value for later
 		
 }
